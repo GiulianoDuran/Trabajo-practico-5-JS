69525ffd82d9e2802c40072ec9c029a7f9ccb175
Giuliano Duran <durangiulianoepet20@gmail.com>
2022-10-06 10:25:06 -0300

    Mostrando Valores

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const c = 7;
 var suma = a + c + b;
 let multiplicacion = a * b * c; 
 const resta = c - a - b;
+
+console.log ("El valor de la suma es:",suma);
+console.log ("El valor de la multiplicacion es:",multiplicacion);
+console.log ("El valor de la resta es:",resta);
